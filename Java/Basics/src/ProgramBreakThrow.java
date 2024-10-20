d32d7ae088052f738eba14a1a4d1a4731429793c
Tuhin ahamed <mernstackprogramming@gmail.com>
2024-10-20 12:35:43 +0600

    Java Program break throw

---
+++ Java/Basics/src/ProgramBreakThrow.java
@@ -0,0 +1,20 @@
+public class ProgramBreakThrow {
+    public static void main(String[] args) {
+        System.out.println("Program break throw");
+
+    }
+}
+
+/*
+Java Program break throw
+public: public is an access modifier.
+class: class is a user defined blueprint.
+ProgramBreakThrow : ProgramBreakThrow is the Name of the class.
+static : static allows us to call method without creating classes.
+void: void is called the return type. void means null or empty.
+main: main is the name of function.
+(String[] args) : (String[] args) is called command line arguments
+System: System is a built-in class in java
+out: out is used for output
+println: println means in a new line.
+ */
