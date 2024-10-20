112c550ca36a668ece89ef62364b6c426424ec22
Tuhin ahamed <mernstackprogramming@gmail.com>
2024-10-20 15:11:55 +0600

    create and call mehtods

---
+++ Java/OOPs/src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    // method declaration
+    public static void printHello(){
+        System.out.println("Hello World using Methods");
+    }
+    public static void main(String[] args) {
+        System.out.println("Methods in java");
+        // method call
+        printHello();
+    }
+}
