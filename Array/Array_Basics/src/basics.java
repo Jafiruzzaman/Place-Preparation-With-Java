b2f588131ff8fa9d25be3dddc6cef8a9fbe25363
Tuhin ahamed <mernstackprogramming@gmail.com>
2024-10-20 15:17:39 +0600

    basics of array

---
+++ Array/Array_Basics/src/basics.java
@@ -0,0 +1,25 @@
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("Basics of array");
+//        create an array
+//        way 1
+        int[] arr = {10, 20, 30, 40, 50};
+        int array[] = new int[5];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+
+    }
+
+}
+/*
+Array: Array is a collection of similar data in contiguous format
+
+ */
